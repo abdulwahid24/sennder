a4eed60d2115c1587799aa7cfdbd2ffaca21b06c
Abdulwahid Barguzar <abdulwahid24@gmail.com>
2020-11-19 22:28:58 +0100

    Fixed angular prod build issue

--- webapp/src/app/config/environments.ts
+++ webapp/src/app/config/environments.ts
@@ -3,8 +3,8 @@ export const API_ENDPOINT = get_endpoint();
 function get_endpoint() {
   switch (window.location.hostname) {
         case 'localhost':
-                return 'http://localhost:8000/'
+                return 'http://localhost:8000'
         case 'sennder.abdulwahid.info':
-                return 'https://api.sennder.abdulwahid.info/';
+                return 'https://api.sennder.abdulwahid.info';
       }
 }
