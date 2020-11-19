1b5e0f4eecf99740f71e4c59b81e7ba9c1c7d004
Abdulwahid Barguzar <abdulwahid24@gmail.com>
2020-11-19 19:15:42 +0100

    replaced world with sennder

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'world'`, () => {
+  it(`should have as title 'sennder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('world');
+    expect(app.title).toEqual('sennder');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('world app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sennder app is running!');
   });
 });
